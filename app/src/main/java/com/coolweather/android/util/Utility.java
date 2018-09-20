94bd8124cc693681b08a16f04c93132d1720f440
klay.yu <klay.yu@picovr.com>
2018-09-20 15:41:07 +0800

    完成遍历省市县数据及展示的功能

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,89 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.Country;
+import com.coolweather.android.db.Province;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try
+            {
+                JSONArray allProvince=new JSONArray(response);
+                for(int i=0;i<allProvince.length();i++)
+                {
+                    JSONObject provinceObject=allProvince.getJSONObject(i);
+                    Province province=new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response,int provinceId)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try
+            {
+                JSONArray allProvince=new JSONArray(response);
+                for(int i=0;i<allProvince.length();i++)
+                {
+                    JSONObject provinceObject=allProvince.getJSONObject(i);
+                    City city=new City();
+                    city.setCityName(provinceObject.getString("name"));
+                    city.setCityCode(provinceObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCountryResponse(String response,int cityId)
+    {
+        if(!TextUtils.isEmpty(response))
+        {
+            try
+            {
+                JSONArray allProvince=new JSONArray(response);
+                for(int i=0;i<allProvince.length();i++)
+                {
+                    JSONObject provinceObject=allProvince.getJSONObject(i);
+                    Country country=new Country();
+                    country.setCountryName(provinceObject.getString("name"));
+                    country.setWeatherId(provinceObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
